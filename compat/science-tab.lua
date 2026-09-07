908adbdb09d682a5e1f21e96a4111b8e5b2ab1a3
kryzeth <kryzeth@outlook.com>
2026-09-07 03:54:51 -0700

    Annoying Krastorio 2 fixes for Science Group compat

--- compat/science-tab.lua
+++ compat/science-tab.lua
@@ -1,6 +1,10 @@
 -- early exit script if Science Group mod is not enabled
 if not mods["science-tab"] then return end
 
-data.raw["item-subgroup"]["maraxsis-deepsea-research"].group = "science"
-data.raw["item-subgroup"]["maraxsis-empty-research-vessel"].group = "science"
-data.raw["item-subgroup"]["maraxsis-fill-research-vessel"].group = "science"
+if mods["Krastorio2-spaced-out"] or mods["Krastorio2"] then
+    data.raw["item-subgroup"]["kr-tech-cards-cooling"].group = "science"
+else
+    data.raw["item-subgroup"]["maraxsis-deepsea-research"].group = "science"
+    data.raw["item-subgroup"]["maraxsis-empty-research-vessel"].group = "science"
+    data.raw["item-subgroup"]["maraxsis-fill-research-vessel"].group = "science"
+end
