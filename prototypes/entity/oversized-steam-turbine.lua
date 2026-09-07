1dcf3f264fe6fec222ad041630f8c25ffef153b2
kryzeth <kryzeth@outlook.com>
2026-09-07 04:22:51 -0700

    Reduced oversized steam turbine smoke particle opacity from 180% to 60%, from unreleased v1.34.52

--- prototypes/entity/oversized-steam-turbine.lua
+++ prototypes/entity/oversized-steam-turbine.lua
@@ -270,7 +270,7 @@ data:extend {{
                 south_position = {0.0, -1.0},
                 east_position = {0.75, -0.75},
                 west_position = {0.75, -0.75},
-                frequency = 18000,
+                frequency = 6000,
                 starting_vertical_speed = 0.08,
                 starting_frame_deviation = 60
             },
@@ -280,7 +280,7 @@ data:extend {{
                 south_position = {0.0, -1.0 + 2},
                 east_position = {0.75 + 2, -0.75},
                 west_position = {0.75 + 2, -0.75},
-                frequency = 18000,
+                frequency = 6000,
                 starting_vertical_speed = 0.08,
                 starting_frame_deviation = 60
             },
@@ -290,7 +290,7 @@ data:extend {{
                 south_position = {0.0, -1.0 - 2},
                 east_position = {0.75 - 2, -0.75},
                 west_position = {0.75 - 2, -0.75},
-                frequency = 18000,
+                frequency = 6000,
                 starting_vertical_speed = 0.08,
                 starting_frame_deviation = 60
             }
