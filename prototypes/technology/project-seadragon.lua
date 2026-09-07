cc6a8111f86ddaf1ad5af8a3634b96ee431f6096
kryzeth <kryzeth@outlook.com>
2026-09-06 22:25:40 -0700

    Removed an unnecessary copied rocket silo override, from v1.34.48

--- prototypes/technology/project-seadragon.lua
+++ prototypes/technology/project-seadragon.lua
@@ -128,7 +128,6 @@ for surface in pairs(maraxsis_constants.MARAXSIS_SURFACES) do
             disabled_when_recipe_not_researched = true,
             placeable_by = {{item = "rocket-silo", count = 1}},
             flags = {"placeable-player", "player-creation", "not-in-made-in"},
-            logistic_trash_inventory_size = 1,
             hidden = true,
             hidden_in_factoriopedia = true,
         },
