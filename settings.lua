10f4b30444e35e9547e11e50c677a43bd47f6964
kryzeth <kryzeth@outlook.com>
2026-09-07 19:06:37 -0700

    Hidden default setting for people who eventually update from 2.0 to 2.1, using this mod

--- settings.lua
+++ settings.lua
@@ -1,17 +1,29 @@
-data:extend {{
-    type = "double-setting",
-    name = "maraxsis-water-opacity",
-    setting_type = "startup",
-    default_value = 255,
-    minimum_value = 0,
-    maximum_value = 255,
-    order = "b"
-}}
-
-data:extend {{
-    type = "bool-setting",
-    name = "maraxsis-add-hydraulic-science",
-    setting_type = "startup",
-    default_value = true,
-    order = "a"
-}}
+data:extend {
+    {
+        type = "bool-setting",
+        name = "maraxsis-add-hydraulic-science",
+        setting_type = "startup",
+        default_value = true,
+        order = "a"
+    },
+    {
+        type = "double-setting",
+        name = "maraxsis-water-opacity",
+        setting_type = "startup",
+        default_value = 255,
+        minimum_value = 0,
+        maximum_value = 255,
+        order = "b"
+    },
+    -- theoretically, if people update from 2.0 to 2.1 using Maraxsis Classic,
+    -- this setting should end up disabled by default, just to reduce unnecessary prototypes
+    {
+        type = "bool-setting",
+        name = "maraxsis-migration-mode",
+        setting_type = "startup",
+        hidden = true,
+        default_value = false,
+        forced_value = false,
+        order = "d"
+    },
+}
